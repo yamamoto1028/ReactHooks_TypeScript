d132ba2d8d2415a9d5a13ce48d576f045c1e3823
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2023-12-27 22:03:09 +0900

    ステップ１（提出１回目）

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import "./App.css";
 import { BookToRead } from "./BookToRead";
 
+//インポート文追加
+import BookRow from "./BookRow";
+
 const dummyBooks: BookToRead[] = [
   {
     id: 1,
@@ -24,15 +27,24 @@ const dummyBooks: BookToRead[] = [
 ];
 
 const App = () => {
+  const bookRows = dummyBooks.map((b) => {
+    return (
+      <BookRow
+        book={b}
+        key={b.id}
+        onMemoChange={(id, memo) => {}}
+        onDelete={(id) => {}}
+      />
+    );
+  });
+  
   return (
     <div className="App">
       <section className="nav">
         <h1>読みたい本リスト</h1>
         <div className="button-like">本を追加</div>
       </section>
-      <section className="main">
-        <h1>チュートリアルを始めましょう</h1>
-      </section>
+      <section className="main">{bookRows}</section>
     </div>
   );
 };
